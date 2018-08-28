975198d8e8ed3078774b5ca34502ddb87a4a9a8f
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-28 15:15:20 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-Assignments/m2/Assignment-2/Solution.java
+++ CSPP2-Assignments/m2/Assignment-2/Solution.java
@@ -6,7 +6,8 @@ public class Solution {
         This finds the roots of function
     */
     public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-    int dis = (b*b) - (4 * a * c);
+    int four = 4;
+    int dis = (b*b) - (four * a * c);
     double rootOne = (-b + (Math.sqrt(dis))) / (2 * a);
     double rootTwo = (-b - (Math.sqrt(dis))) / (2 * a);
     System.out.print(rootOne + " " + rootTwo);
