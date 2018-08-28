d0df8c137ed0b590cb793287d0dc6a5d66ef2bbd
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-28 22:01:46 +0530

    comparsions

---
+++ CSPP2-Practice/m2/bigger_equal_smaller.java
@@ -0,0 +1,15 @@
+class bigger_equal_smaller {
+  public static void main(String args[]) {
+    int a = 15;
+    int b = 20;
+    if(a>b) {
+    	System.out.println("Bigger");
+    }
+    else if (a<b) {
+    	System.out.println("Smaller");
+    }
+    else {
+    	System.out.println("Equal");
+    }
+   }
+ }
