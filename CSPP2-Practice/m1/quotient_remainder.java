6ec862118cf8eb0550d3a3cea5370c98ace781a9
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-28 22:25:15 +0530

    quo and rem

---
+++ CSPP2-Practice/m1/quotient_remainder.java
@@ -0,0 +1,9 @@
+class quotient_remainder {
+    public static void main(String args[]) {
+        int a = 89;
+        int quo = (89)/10;
+        int remaind = 89%10;
+        System.out.println(quo);
+        System.out.println(remaind);
+    }
+}
