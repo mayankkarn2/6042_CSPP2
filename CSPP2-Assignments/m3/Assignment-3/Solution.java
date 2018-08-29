c2de66a9b23dedcfcb889acd184b16e4928de745
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-29 13:43:10 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m3/Assignment-3/Solution.java
+++ CSPP2-Assignments/m3/Assignment-3/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
         int gcdVa = 1;
         for (int i = 2; i <= n2; i++) {
             if(n1%i == 0 && n2%i == 0) {
-                int gcd = i;
+                gcdVa = i;
             }
         }
         return gcdVa;
