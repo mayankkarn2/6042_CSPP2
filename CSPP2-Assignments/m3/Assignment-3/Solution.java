40aab455a1c75e498a826e1d42a86e464e0c56de
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-29 13:46:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m3/Assignment-3/Solution.java
+++ CSPP2-Assignments/m3/Assignment-3/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
     }
-    public static int gcd(int n1,int n2) {
+    public static int gcd(final int n1, final int n2) {
         int gcdVa = 1;
         for (int i = 2; i <= n2; i++) {
             if (n1 % i == 0 && n2 % i == 0) {
