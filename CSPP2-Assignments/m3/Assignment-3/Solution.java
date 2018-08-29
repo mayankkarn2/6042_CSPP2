e35b1c88fdfd4ec068dd76e9f0d4ffd6488c32b0
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-29 13:48:01 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m3/Assignment-3/Solution.java
+++ CSPP2-Assignments/m3/Assignment-3/Solution.java
@@ -8,9 +8,9 @@ public class Solution {
     private Solution() {
         //Nothing happens here
     }
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        Scanner s = new Scanner(System.in);   
+        Scanner s = new Scanner(System.in);  
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1, n2));
