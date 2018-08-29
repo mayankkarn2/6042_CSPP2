d95443bc273617bb95a907fee24aa7ff142b365d
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-29 12:19:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m3/Assignment-2/Solution.java
+++ CSPP2-Assignments/m3/Assignment-2/Solution.java
@@ -16,7 +16,7 @@ final class Solution {
         //Nothing happens here.
     }
     public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);  
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         for (int i = 1; i <= n; i++) {
