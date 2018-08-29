6b48c3e171129324302b361cedfc861fcf44f39d
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-08-29 12:11:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Assignments/m3/Assignment-2/Solution.java
+++ CSPP2-Assignments/m3/Assignment-2/Solution.java
@@ -1,34 +1,41 @@
 
 import java.util.Scanner;
-/*
-	Do not modify this main function.
-	*/
+/**
+ * Class for solution.
+ */
 public class Solution {
-/* Fill the main function to print the number of 7's between 1 to n*/
-    public static void main(String[] args) {
+/**
+ * { function_description }
+ *
+ * @param      args  The arguments
+ */
+/**
+ * { item_description }
+ */
+    private Solution() {
+        //Nothing happens here.
+    }
+    public static void main(final String[] args) {
 
-	    Scanner s=new Scanner(System.in);      
-	    int n = s.nextInt();
-	    int count = 0;
-	    for(int i = 1; i <= n; i++) {
-	    	String number = Integer.toString(i);
-	    	// System.out.println(number);
-	    	boolean isFound = number.contains("7");
-	    	// System.out.println(isFound);
-	    	// System.out.println("-----------------");
-	    	if(isFound == true) {
-	    		char[] numb = number.toCharArray();
-	    		int count7 = 0;
-	    		for(char num : numb) {
-	    			if(num == '7') {
-	    				count7 += 1;
-	    			}
-	    		}
-	    		count += count7;
-	    	}
-	   
-	    }
-	    System.out.println(count);
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+            String number = Integer.toString(i);
+            boolean isFound = number.contains("7");
+            if(isFound == true) {
+                char[] numb = number.toCharArray();
+                int count7 = 0;
+                for(char num : numb) {
+                    if(num == '7') {
+                        count7 += 1;
+                    }
+                }
+                count += count7;
+            }
+       
+        }
+        System.out.println(count);
         
     }
 }
